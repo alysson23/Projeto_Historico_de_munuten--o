3eacd08ba6549b3133c87b0fe17c135ca10c7535
alysson23 <86750859+alysson23@users.noreply.github.com>
2021-09-19 16:50:32 -0300

    Código com for

--- historico.py
+++ historico.py
@@ -4,8 +4,9 @@ x = ''
 while True:
     arquivo = pd.read_excel(r"C:\Users\Alysson\OneDrive\Desktop/planilha.xlsx")
     x = input(str("Digite o nome do produto "))
-    dados_filtrados = arquivo["Produtos"] == x
-    isempty = dados_filtrados.empty
-    print('Is the DataFrame empty :', isempty)
-    if isempty == False:
-        print(arquivo[dados_filtrados])
+    dados_filtrados = arquivo["Produtos"]
+    for c in dados_filtrados:
+        if x == c:
+            dados_filtrados = arquivo["Produtos"] == x
+            print(arquivo[dados_filtrados])
+            arquivo[dados_filtrados].clear()
