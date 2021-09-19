d1532e8e030e9230f9def60a7595c92fcbc3fc69
alysson23 <86750859+alysson23@users.noreply.github.com>
2021-09-19 17:25:28 -0300

    Update historico.py

--- historico.py
+++ historico.py
@@ -1,12 +1,17 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 x = ''
 while True:
-    arquivo = pd.read_excel(r"C:\Users\Alysson\OneDrive\Desktop/planilha.xlsx")
+    arquivo = pd.read_excel(C:\Users\Alysson\OneDrive\Documents\meuprojtoGitHub\Projeto_Historico_de_munuten--o/planilha)
     x = input(str("Digite o nome do produto "))
     dados_filtrados = arquivo["Produtos"]
     for c in dados_filtrados:
         if x == c:
             dados_filtrados = arquivo["Produtos"] == x
             print(arquivo[dados_filtrados])
-            arquivo[dados_filtrados].clear()
+            break
+    print("Não contém")
+    resposta = " "
+    while resposta not in "SN":
+        resposta = str(input("Você deseja continuar [S/N] ")).strip().upper()[0]
+    if resposta == "N":
+        break
